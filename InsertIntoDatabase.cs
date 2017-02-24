dda23031645ab68fb59cb4a06ba1b550e652bbf8
blc25 <blc25@students.ptcollege.edu>
2017-02-24 14:58:07 -0500

    Add files via upload

---
+++ InsertIntoDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for InsertIntoDatabase
+/// </summary>
+public class InsertIntoDatabase
+{
+    public void InsertInto(String DBConnectString, string[] values, string table)
+    {
+        string insertValuesString = null; 
+        
+        for (int i = 0; i < values.Length; i++)
+        {
+            if(i!=values.Length-1)
+            insertValuesString += values[i] + ", ";
+            else
+                insertValuesString += values[i];
+        }
+        string TableInfo = "INSERT Into " + table + " VALUES("+insertValuesString+");";
+        SqlConnection dbConnection = new SqlConnection(DBConnectString);
+        try
+        {
+            dbConnection.Open();
+            dbConnection.ChangeDatabase("OlympicAlienAndTheCram");
+        }
+        catch
+        {
+            return;
+        }
+        SqlCommand studentSchedule = new SqlCommand(TableInfo, dbConnection);
+        studentSchedule.ExecuteNonQuery();
+    }
+}
