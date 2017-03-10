db30240103794a8305f5b36021be74ce52e279ad
blc25 <blc25@students.ptcollege.edu>
2017-03-10 15:00:02 -0500

    Add files via upload

---
+++ FloodDatabase.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FloodDatabase.aspx.cs" Inherits="FloodDatabase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:ListBox ID="ChoiceBox" runat="server" OnSelectedIndexChanged="ChoiceBox_SelectedIndexChanged">
+            <asp:ListItem>Classroom</asp:ListItem>
+            <asp:ListItem>Instructor</asp:ListItem>
+        </asp:ListBox>
+        <p>
+            <asp:Button ID="FillDatabase" runat="server" OnClick="Button1_Click" Text="FillDatabase" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ FloodDatabase.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FloodDatabase : System.Web.UI.Page
+{
+    string fileLocation, table, dbconnectionString;
+    int numberOfValues;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dbconnectionString = "Data Source=stusql;Integrated Security=true";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        FileUsageForDatabase fileuse = new FileUsageForDatabase();
+        if (ChoiceBox.SelectedItem.Text == "Classroom")
+        {
+            table = "Classroom";
+            numberOfValues = 4;
+            fileLocation = "F:\\QUARTER 6\\Database Access\\App_Code\\Classroom.txt";
+            fileuse.fillDatabase(fileLocation, table, dbconnectionString, numberOfValues);
+        }
+        else if (ChoiceBox.SelectedItem.Text == "Instructor")
+        {
+            table = "Instructor";
+            numberOfValues = 2;
+            fileLocation = "F:\\QUARTER 6\\Database Access\\App_Code\\Instructor.txt";
+            fileuse.fillDatabase(fileLocation, table, dbconnectionString, numberOfValues);
+        }
+    }
+
+    protected void ChoiceBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
